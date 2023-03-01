d485bfc104664f052ecff06830acc7f1c66634f2
Hooman Edraki <66573756+hmnedi@users.noreply.github.com>
2023-03-02 01:52:05 +0330

    Update CardLaout.java

--- 1- Greed/src/CardLaout.java
+++ 1- Greed/src/CardLaout.java
@@ -4,10 +4,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class CardLaout extends JFrame implements ActionListener {
-    private CardLayout cardLayout;
-    private JPanel panelGame;
-    private JButton btnSinglePlayer, btnExit, btnSetting;
-    private Container container;
+    CardLayout cardLayout;
+    JButton btnSinglePlayer, btnExit, btnSetting;
+    Container container;
 
     public CardLaout() {
 
@@ -19,12 +18,10 @@ public class CardLaout extends JFrame implements ActionListener {
         this.setTitle("Greedy Eating Number");
         this.setResizable(false);
         this.getContentPane().setBackground(new Color(0x5b5870));
-        this.setLocationRelativeTo(null);
         ImageIcon logo = new ImageIcon("pic/logo.png");
         this.setIconImage(logo.getImage());
 
-        panelGame = new JPanel();
-
+        // setting up the menu page
         btnSinglePlayer = new JButton("Single Player");
         btnSetting = new JButton("Setting");
         btnExit = new JButton("Exit");
@@ -33,13 +30,13 @@ public class CardLaout extends JFrame implements ActionListener {
         btnSetting.addActionListener(this);
         btnExit.addActionListener(this);
 
-        btnSinglePlayer.setFont(new Font("Inkpen2 Script Std", Font.PLAIN, 36)); //TODO: auto install font
-        btnSetting.setFont(new Font("Inkpen2 Script Std", Font.PLAIN, 38));
-        btnExit.setFont(new Font("Inkpen2 Script Std", Font.PLAIN, 32));
+        btnSinglePlayer.setFont(new Font("Inkpen2 Script Std", Font.PLAIN, 46)); //TODO: auto install font
+        btnSetting.setFont(new Font("Inkpen2 Script Std", Font.PLAIN, 48));
+        btnExit.setFont(new Font("Inkpen2 Script Std", Font.PLAIN, 44));
 
-        btnSinglePlayer.setBounds(275, 80 + 80, 200, 80);
-        btnSetting.setBounds(275, 180 + 80, 200, 80);
-        btnExit.setBounds(285 + 30, 282 + 80, 120, 50);
+        btnSinglePlayer.setBounds(375, 250, 300, 100);
+        btnSetting.setBounds(375, 375 , 300, 100);
+        btnExit.setBounds(425, 302 + 200, 200, 70);
 
         btnSetting.setFocusable(false);
         btnSinglePlayer.setFocusable(false);
@@ -54,91 +51,25 @@ public class CardLaout extends JFrame implements ActionListener {
 
         // simple trick to set background image, I'm using a label as the panel
         ImageIcon pageMenuBack = new ImageIcon("pic/backpic.jpg");
-        JLabel panelMenu = new JLabel(pageMenuBack);
+        JLabel panelMenu = new JLabel(pageMenuBack), lblGameTittle = new JLabel("Milk Shake's Greed");
+
+        lblGameTittle.setBounds(300, 120, 450, 50);
+        lblGameTittle.setHorizontalAlignment(SwingConstants.CENTER);
+        lblGameTittle.setVerticalAlignment(SwingConstants.CENTER);
+        lblGameTittle.setFont(new Font("Inkpen2 Script Std", Font.BOLD, 62));
+        lblGameTittle.setForeground(new Color(0x544633));
 
         panelMenu.setLayout(null);
+        panelMenu.add(lblGameTittle);
         panelMenu.add(btnSinglePlayer);
         panelMenu.add(btnSetting);
         panelMenu.add(btnExit);
 
-        container.add("Panel Menu", panelMenu);
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == btnSinglePlayer) {
-            cardLayout.next(container);
-        }
-        else if (e.getSource() == btnExit){
-            System.exit(0);
-        }
-
-    }
-}
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-public class CardLaout extends JFrame implements ActionListener {
-    private CardLayout cardLayout;
-    private JPanel panelGame;
-    private JButton btnSinglePlayer, btnExit, btnSetting;
-    private Container container;
-
-    public CardLaout() {
-
-        container = getContentPane();
-        cardLayout = new CardLayout();
-        container.setLayout(cardLayout);
-
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("Greedy Eating Number");
-        this.setResizable(false);
-        this.getContentPane().setBackground(new Color(0x5b5870));
-        this.setLocationRelativeTo(null);
-        ImageIcon logo = new ImageIcon("pic/logo.png");
-        this.setIconImage(logo.getImage());
-
-        panelGame = new JPanel();
-
-        btnSinglePlayer = new JButton("Single Player");
-        btnSetting = new JButton("Setting");
-        btnExit = new JButton("Exit");
-
-        btnSinglePlayer.addActionListener(this);
-        btnSetting.addActionListener(this);
-        btnExit.addActionListener(this);
-
-        btnSinglePlayer.setFont(new Font("Inkpen2 Script Std", Font.PLAIN, 36)); //TODO: auto install font
-        btnSetting.setFont(new Font("Inkpen2 Script Std", Font.PLAIN, 38));
-        btnExit.setFont(new Font("Inkpen2 Script Std", Font.PLAIN, 32));
-
-        btnSinglePlayer.setBounds(275, 80 + 80, 200, 80);
-        btnSetting.setBounds(275, 180 + 80, 200, 80);
-        btnExit.setBounds(285 + 30, 282 + 80, 120, 50);
-
-        btnSetting.setFocusable(false);
-        btnSinglePlayer.setFocusable(false);
-        btnExit.setFocusable(false);
-
-        btnSinglePlayer.setBackground(new Color(0xdbbc91));
-        btnSinglePlayer.setBorder(BorderFactory.createLineBorder(new Color(0x544633)));
-        btnSetting.setBackground(new Color(0xdbbc91));
-        btnSetting.setBorder(BorderFactory.createLineBorder(new Color(0x544633)));
-        btnExit.setBackground(new Color(0xdbbc91));
-        btnExit.setBorder(BorderFactory.createLineBorder(new Color(0x544633)));
-
-        // simple trick to set background image, I'm using a label as the panel
-        ImageIcon pageMenuBack = new ImageIcon("pic/backpic.jpg");
-        JLabel panelMenu = new JLabel(pageMenuBack);
-
-        panelMenu.setLayout(null);
-        panelMenu.add(btnSinglePlayer);
-        panelMenu.add(btnSetting);
-        panelMenu.add(btnExit);
+        // calling the panelGame from Game class
+        JPanel panelGame = new Game().frame;
 
         container.add("Panel Menu", panelMenu);
+        container.add("Panel Game", panelGame);
     }
 
     @Override
