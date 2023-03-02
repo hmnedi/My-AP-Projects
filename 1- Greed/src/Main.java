f7fdadbfa1cd5b3b1072f4b9cb5806318cf1ff91
Hooman Edraki <66573756+hmnedi@users.noreply.github.com>
2023-03-02 15:05:27 +0330

    Create Main.java

---
+++ 1- Greed/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        CardLaout frame = new CardLaout();
+        frame.setSize(750 + 300, 500 + 200);
+        frame.setLocationRelativeTo(null);
+
+        frame.setVisible(true);
+    }
+}
