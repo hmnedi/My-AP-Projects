f621bcfed234e5ae35e49b5a6c65d0f012b23474
Hooman Edraki <66573756+hmnedi@users.noreply.github.com>
2023-03-02 15:04:58 +0330

    Update Game.java

--- 1- Greed/src/Game.java
+++ 1- Greed/src/Game.java
@@ -6,8 +6,7 @@ import java.util.Random;
 
 public class Game {
     int n = 19, iPos, jPos;
-    Border selectionBorder = BorderFactory.createLineBorder(new Color(0x544633), 2),
-            removeBorder = BorderFactory.createEmptyBorder();
+    Border selectionBorder = BorderFactory.createLineBorder(new Color(0x544633), 2);
     JPanel frame, panelTopNavigationBar, panelMid;
     JLabel[][] lblNumbers;
     Action upAaction, downAction, leftAction, rightAction, upRightAction,
@@ -129,10 +128,38 @@ public class Game {
             }
         }
 
+        // d - right
+        if ((jPos + 1 < 3*n) && (!lblNumbers[iPos][jPos+1].getText().equals("")) && (jPos + Integer.valueOf(lblNumbers[iPos][jPos+1].getText()) < 3*n)) {
+            for (int i=0; i<Integer.valueOf(lblNumbers[iPos][jPos+1].getText()); i++){
+                lblNumbers[iPos][jPos+1+i].setBorder(selectionBorder);
+            }
+        }
+
         // e - up right
         if ((iPos - 1 >= 0) && (jPos + 1 < 3*n) && (!lblNumbers[iPos-1][jPos+1].getText().equals("")) && (iPos - Integer.valueOf(lblNumbers[iPos-1][jPos+1].getText()) >= 0) && (jPos + Integer.valueOf(lblNumbers[iPos-1][jPos+1].getText()) < 3*n)) {
             for (int i=0; i<Integer.valueOf(lblNumbers[iPos-1][jPos+1].getText()); i++){
                 lblNumbers[iPos-1-i][jPos+1+i].setBorder(selectionBorder);
+            }
+        }
+
+        // q - up left
+        if ((iPos - 1 >= 0) && (jPos - 1 >= 0) && (!lblNumbers[iPos-1][jPos-1].getText().equals("")) && (iPos - Integer.valueOf(lblNumbers[iPos-1][jPos-1].getText()) >= 0) && (jPos - Integer.valueOf(lblNumbers[iPos-1][jPos-1].getText()) >= 0)) {
+            for (int i=0; i<Integer.valueOf(lblNumbers[iPos-1][jPos-1].getText()); i++){
+                lblNumbers[iPos-1-i][jPos-1-i].setBorder(selectionBorder);
+            }
+        }
+
+        // c - down right
+        if ((iPos + 1 < n) && (jPos + 1 < 3*n) && (!lblNumbers[iPos+1][jPos+1].getText().equals("")) && (iPos + Integer.valueOf(lblNumbers[iPos+1][jPos+1].getText()) < n) && (jPos + Integer.valueOf(lblNumbers[iPos+1][jPos+1].getText()) < 3*n)) {
+            for (int i=0; i<Integer.valueOf(lblNumbers[iPos+1][jPos+1].getText()); i++){
+                lblNumbers[iPos+1+i][jPos+1+i].setBorder(selectionBorder);
+            }
+        }
+
+        // z - down left
+        if ((iPos + 1 < n) && (jPos - 1 >= 0) && (!lblNumbers[iPos+1][jPos-1].getText().equals("")) && (iPos + Integer.valueOf(lblNumbers[iPos+1][jPos-1].getText()) < n) && (jPos - Integer.valueOf(lblNumbers[iPos+1][jPos-1].getText()) >= 0)) {
+            for (int i=0; i<Integer.valueOf(lblNumbers[iPos+1][jPos-1].getText()); i++){
+                lblNumbers[iPos+1+i][jPos-1-i].setBorder(selectionBorder);
             }
         }
     }
@@ -140,7 +167,7 @@ public class Game {
     private void clearHighLights() {
         for(int i=0; i<n; i++)
             for(int j=0; j<3*n; j++)
-                lblNumbers[i][j].setBorder(removeBorder);
+                lblNumbers[i][j].setBorder(null);
     }
 
     private void changePlayerPos(int i, int j) {
@@ -190,7 +217,6 @@ public class Game {
                 changePlayerPos(iPos + tmp, jPos);
                 showHighLight();
             }
-
         }
     }
 
@@ -214,49 +240,85 @@ public class Game {
     public class RightAction extends AbstractAction {
         @Override
         public void actionPerformed(ActionEvent e) {
-//            label.setLocation(label.getX()+10, label.getY());
-            System.out.println("hee");
-
+            System.out.println("d");
+            if (jPos + 1 < 3*n && lblNumbers[iPos][jPos+1].getBorder() == selectionBorder){
+                int tmp = Integer.valueOf(lblNumbers[iPos][jPos+1].getText());
+                for (int i=0; i<tmp; i++){
+                    lblNumbers[iPos][jPos+1+i].setText("");
+                    lblNumbers[iPos][jPos+1+i].setIcon(null);
+                    lblNumbers[iPos][jPos+1+i].setBackground(Color.WHITE);
+                }
+                changePlayerPos(iPos, jPos + tmp);
+                showHighLight();
+            }
         }
     }
 
     public class UpRightAction extends AbstractAction {
         @Override
         public void actionPerformed(ActionEvent e) {
-//            label.setLocation(label.getX()+10, label.getY()-10);
-            System.out.println("hee");
-
+            System.out.println("e");
+            if (iPos - 1 >= 0 && jPos + 1 < 3*n && lblNumbers[iPos-1][jPos+1].getBorder() == selectionBorder){
+                int tmp = Integer.valueOf(lblNumbers[iPos-1][jPos+1].getText());
+                for (int i=0; i<tmp; i++){
+                    lblNumbers[iPos-1-i][jPos+1+i].setText("");
+                    lblNumbers[iPos-1-i][jPos+1+i].setIcon(null);
+                    lblNumbers[iPos-1-i][jPos+1+i].setBackground(Color.WHITE);
+                }
+                changePlayerPos(iPos - tmp, jPos + tmp);
+                showHighLight();
+            }
         }
     }
 
     public class UpLeftAction extends AbstractAction {
         @Override
         public void actionPerformed(ActionEvent e) {
-//            label.setLocation(label.getX()-10, label.getY()-10);
-            System.out.println("hee");
-
+            System.out.println("q");
+            if (iPos - 1 >= 0 && jPos - 1 >= 0 && lblNumbers[iPos-1][jPos-1].getBorder() == selectionBorder){
+                int tmp = Integer.valueOf(lblNumbers[iPos-1][jPos-1].getText());
+                for (int i=0; i<tmp; i++){
+                    lblNumbers[iPos-1-i][jPos-1-i].setText("");
+                    lblNumbers[iPos-1-i][jPos-1-i].setIcon(null);
+                    lblNumbers[iPos-1-i][jPos-1-i].setBackground(Color.WHITE);
+                }
+                changePlayerPos(iPos - tmp, jPos - tmp);
+                showHighLight();
+            }
         }
     }
 
     public class DownRightAction extends AbstractAction {
         @Override
         public void actionPerformed(ActionEvent e) {
-//            label.setLocation(label.getX()+10, label.getY()+10);
-            System.out.println("hee");
-
+            System.out.println("c");
+            if (iPos + 1 < n && jPos + 1 < 3*n && lblNumbers[iPos+1][jPos+1].getBorder() == selectionBorder){
+                int tmp = Integer.valueOf(lblNumbers[iPos+1][jPos+1].getText());
+                for (int i=0; i<tmp; i++){
+                    lblNumbers[iPos+1+i][jPos+1+i].setText("");
+                    lblNumbers[iPos+1+i][jPos+1+i].setIcon(null);
+                    lblNumbers[iPos+1+i][jPos+1+i].setBackground(Color.WHITE);
+                }
+                changePlayerPos(iPos + tmp, jPos + tmp);
+                showHighLight();
+            }
         }
     }
 
     public class DownLeftAction extends AbstractAction {
         @Override
         public void actionPerformed(ActionEvent e) {
-//            label.setLocation(label.getX()-10, label.getY()+10);
-            System.out.println("hee");
-
+            System.out.println("z");
+            if (iPos + 1 < n && jPos - 1 >= 0 && lblNumbers[iPos+1][jPos-1].getBorder() == selectionBorder){
+                int tmp = Integer.valueOf(lblNumbers[iPos+1][jPos-1].getText());
+                for (int i=0; i<tmp; i++){
+                    lblNumbers[iPos+1+i][jPos-1-i].setText("");
+                    lblNumbers[iPos+1+i][jPos-1-i].setIcon(null);
+                    lblNumbers[iPos+1+i][jPos-1-i].setBackground(Color.WHITE);
+                }
+                changePlayerPos(iPos + tmp, jPos - tmp);
+                showHighLight();
+            }
         }
     }
-
-
-
-
 }
