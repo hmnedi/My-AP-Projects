39f81e2abd541d4d078b7dbeff292fd22a18cdd1
Hooman Edraki <66573756+hmnedi@users.noreply.github.com>
2023-03-03 14:21:52 +0330

    Update CardLaout.java

--- 1- Greed/src/CardLaout.java
+++ 1- Greed/src/CardLaout.java
@@ -1,12 +1,22 @@
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
-public class CardLaout extends JFrame implements ActionListener {
+public class CardLaout extends JFrame implements ActionListener, ChangeListener {
     CardLayout cardLayout;
-    JButton btnSinglePlayer, btnExit, btnSetting;
+    JButton btnSinglePlayer, btnExit, btnSetting, btnBack;
     Container container;
+    JPanel panelSetting;
+    JSlider sliderValueN;
+    JLabel lblSliderN;
 
     public CardLaout() {
 
@@ -15,7 +25,7 @@ public class CardLaout extends JFrame implements ActionListener {
         container.setLayout(cardLayout);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("Greedy Eating Number");
+        this.setTitle("Milk Shake's Greed"); // todo: or milkshakes grid
         this.setResizable(false);
         this.getContentPane().setBackground(new Color(0x5b5870));
         ImageIcon logo = new ImageIcon("pic/logo.png");
@@ -59,6 +69,36 @@ public class CardLaout extends JFrame implements ActionListener {
         lblGameTittle.setFont(new Font("Inkpen2 Script Std", Font.BOLD, 62));
         lblGameTittle.setForeground(new Color(0x544633));
 
+        // read file and load game's records
+        String data = "<html>Records<hr/>";
+        try {
+            File Obj = new File("logs/history.gametxt");
+            Scanner Reader = new Scanner(Obj);
+
+            while (Reader.hasNextLine()) {
+                data += "- " + Reader.nextLine() + "<br/>";
+            }
+            Reader.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("An error has occurred.");
+            e.printStackTrace();
+        }
+        data += "</html>";
+
+        JLabel lblRecords = new JLabel(data);
+        lblRecords.setVerticalAlignment(SwingConstants.TOP);
+        lblRecords.setHorizontalAlignment(SwingConstants.CENTER);
+        lblRecords.setFont(new Font("Inkpen2 Script Std", Font.PLAIN, 20));
+        lblRecords.setForeground(new Color(0x544633));
+
+        JScrollPane scroller = new JScrollPane(lblRecords, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scroller.getViewport().setBackground(new Color(0x77DBBC91, true));
+        scroller.setBorder(BorderFactory.createLineBorder(new Color(0x544633)));
+        scroller.setBounds(30, 30, 260, 600);
+
+
+        panelMenu.add(scroller);
         panelMenu.setLayout(null);
         panelMenu.add(lblGameTittle);
         panelMenu.add(btnSinglePlayer);
@@ -68,18 +108,83 @@ public class CardLaout extends JFrame implements ActionListener {
         // calling the panelGame from Game class
         JPanel panelGame = new Game().frame;
 
+        // setting up panel setting, it could have been in another class if I wanted to add music
+        panelSetting = new JPanel();
+        panelSetting.setBackground(new Color(0xf7e4c8));
+        panelSetting.setLayout(null);
+        btnBack = new JButton("Back");
+        btnBack.setBounds(15, 15, 70, 25);
+        btnBack.setFont(new Font("Inkpen2 Script Std", Font.PLAIN, 22));
+        btnBack.setForeground(new Color(0x595959));
+        btnBack.setBackground(new Color(0xF1D7BD));
+        btnBack.addActionListener(this);
+        btnBack.setFocusable(false);
+        JLabel lblPleaseRestart = new JLabel("Please Restart the game after any changes");
+        lblPleaseRestart.setBounds(300, 300, 500, 100);
+        lblPleaseRestart.setFont(new Font("Inkpen2 Script Std", Font.PLAIN, 25));
+        lblPleaseRestart.setForeground(new Color(0x544633));
+        lblPleaseRestart.setBackground(null);
+        lblPleaseRestart.setOpaque(false);
+
+        sliderValueN = new JSlider(19, 49, 19);
+        sliderValueN.setBounds(300, 150, 400, 100);
+        sliderValueN.setPaintTicks(true);
+        sliderValueN.setMinorTickSpacing(1);
+        sliderValueN.setBackground(null);
+        sliderValueN.setPaintTrack(true);
+        sliderValueN.setMajorTickSpacing(10);
+        sliderValueN.setPaintLabels(true);
+
+        lblSliderN = new JLabel("19");
+        lblSliderN.setBounds(500, 50, 400, 50);
+        lblSliderN.setFont(new Font("Inkpen2 Script Std", Font.PLAIN, 25));
+        lblSliderN.setForeground(new Color(0x544633));
+        lblSliderN.setBackground(null);
+
+        sliderValueN.addChangeListener(this);
+
+        panelSetting.add(lblSliderN);
+        panelSetting.add(sliderValueN);
+        panelSetting.add(lblPleaseRestart);
+        panelSetting.add(btnBack);
+
+
         container.add("Panel Menu", panelMenu);
         container.add("Panel Game", panelGame);
+        container.add("Panel Setting", panelSetting);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == btnSinglePlayer) {
             cardLayout.next(container);
+            Game.timer.start();
+            // todo: I can hold the state of CardLayout and reset and reload every page when the state changes
+        }
+        else if (e.getSource() == btnSetting) {
+            cardLayout.last(container);
         }
         else if (e.getSource() == btnExit){
             System.exit(0);
         }
+        else if (e.getSource() == btnBack) {
+            cardLayout.first(container);
+        }
 
+    }
+
+    @Override
+    public void stateChanged(ChangeEvent e) {
+        String tmp = String.valueOf(sliderValueN.getValue());
+        lblSliderN.setText(tmp);
+        try {
+            FileWriter Writer = new FileWriter("logs/valueOfN.gametxt");
+            Writer.write(tmp);
+            Writer.close();
+        }
+        catch (IOException ea) {
+            System.out.println("An error has occurred.");
+            ea.printStackTrace();
+        }
     }
 }
