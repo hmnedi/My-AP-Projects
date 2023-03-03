f29805a797ccee2d7c8129e857e0676c56bf1f74
Hooman Edraki <66573756+hmnedi@users.noreply.github.com>
2023-03-03 14:22:21 +0330

    Update Main.java

--- 1- Greed/src/Main.java
+++ 1- Greed/src/Main.java
@@ -1,10 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
+
+        UIManager.put("OptionPane.background", Color.WHITE);
+        UIManager.put("Panel.background", Color.WHITE);
 
         CardLaout frame = new CardLaout();
         frame.setSize(750 + 300, 500 + 200);
         frame.setLocationRelativeTo(null);
 
         frame.setVisible(true);
+        // todo: since I used key bindings I can add a multi Player Mode, maybe a solution to getting n from user
     }
 }
