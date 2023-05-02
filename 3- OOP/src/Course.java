fef8304c4a80d6f781126fc59d7169aa7c7f3fd4
Hooman Edraki <66573756+hmnedi@users.noreply.github.com>
2023-05-02 17:53:46 +0330

    Create Course.java

---
+++ 3- OOP/src/Course.java
@@ -0,0 +1,13 @@
+public class Course {
+    private String name;
+    private int code, units, capacity;
+
+    public Course(String name, int code, int units, int capacity) {
+        this.name = name;
+        this.code = code;
+        this.units = units;
+        this.capacity = capacity;
+    }
+
+
+}
