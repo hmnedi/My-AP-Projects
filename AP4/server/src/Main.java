b230cc241b6d7f96e4bba34ac0db831af4820764
Hooman Edraki <66573756+hmnedi@users.noreply.github.com>
2023-06-07 17:31:01 +0330

    Create Main.java

---
+++ AP4/server/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        
+
+        Connect connect = new Connect();
+        connect.setUrl("university");
+        //connect.setUpTables();
+
+
+
+
+    }
+}
