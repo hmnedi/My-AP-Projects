72a37632f37292338a36e89ffd12e99a8238eac2
Hooman Edraki <66573756+hmnedi@users.noreply.github.com>
2023-06-07 17:36:14 +0330

    Create Main.java

---
+++ AP4/client/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        // get username and pass
+        // check for login and return results
+        // show panels
+
+
+    }
+}
